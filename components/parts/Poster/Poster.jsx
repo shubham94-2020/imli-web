168b538304154da1ec64c71d75c9958c5120aaac
Ashokshekade <apshekade_b21@ce.vjti.ac.in>
2024-01-23 14:01:23 +0530

    added why we are here section

--- components/parts/Poster/Poster.jsx
+++ components/parts/Poster/Poster.jsx
@@ -18,30 +18,87 @@ function Poster() {
   }
   return (
     <>
-    <div className="counier-style">
+      <div className="counier-style">
         <div style={obj}>
-        <div className="description">
+          <div className="description">
             <div>
               <h1>{imageSlide[index].title}</h1>
               <h1>{imageSlide[index].body}</h1>
             </div>
             <div className="carousal-boullt">
-              {
-                imageSlide.map((imageSlide,index)=>(
-                    <span 
-                    key={index} 
-                    onClick={()=>{
-                    gotonextindex(index)
-                    }}></span>
-                ))
-              }
+              {imageSlide.map((imageSlide, index) => (
+                <span
+                  key={index}
+                  onClick={() => {
+                    gotonextindex(index);
+                  }}
+                ></span>
+              ))}
+            </div>
+          </div>
         </div>
-           </div>
+      </div>
+      <div className="about-us">
+        <h1>Why We are Here</h1>
+        <div className="hrline">
+          <img
+            src="https://pehlayakshar.org/wp-content/uploads/2016/02/home_charity2_sep1.png"
+            alt="Separator"
+            title=""
+          />
         </div>
-    </div>
+        
+          <div className="text">
+            
+             
+              <label>
+                Pehlay Akshar Foundation was started in 2008 with the core
+                belief that every student has the right to an excellent
+                education and gain the skills needed to be future-ready.Pehlay
+                Akshar Foundation was started in 2008 with the core belief that
+                every student has the right to an excellent education and gain
+                the skills needed to be future-ready.Pehlay Akshar Foundation
+                was started in 2008 with the core belief that every student has
+                the right to an excellent education and gain the skills needed
+                to be future-ready.
+              </label>
+              
+            
+          </div>
+        
+      </div>
+       <hr />
+      <div className="about-us">
+        <h1>Who we are</h1>
+        <div className="hrline">
+          <img
+            src="https://pehlayakshar.org/wp-content/uploads/2016/02/home_charity2_sep1.png"
+            alt="Separator"
+            title=""
+          />
+        </div>
+        
+          <div className="text">
+            
+             
+              <label>
+                Pehlay Akshar Foundation was started in 2008 with the core
+                belief that every student has the right to an excellent
+                education and gain the skills needed to be future-ready.Pehlay
+                Akshar Foundation was started in 2008 with the core belief that
+                every student has the right to an excellent education and gain
+                the skills needed to be future-ready.Pehlay Akshar Foundation
+                was started in 2008 with the core belief that every student has
+                the right to an excellent education and gain the skills needed
+                to be future-ready.
+              </label>
+              
+            
+          </div>
+        
+      </div>
     </>
-
-  )
+  );
 }
 
 export default Poster
