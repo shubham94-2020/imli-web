9b15888dd0d73396424ace92c34a6014bcb93410
maheshthakare2003 <maheshthakare149@gmail.com>
2024-01-23 10:22:01 +0530

    added image slider navigators and home page routing

--- components/parts/Poster/Poster.jsx
+++ components/parts/Poster/Poster.jsx
@@ -1,47 +1,75 @@
-import React, { useState } from 'react'
-import './poster.css'
+
+import React, { useState } from 'react';
+import './poster.css';
 import imageSlide from '../export_image_object/Image_object';
+
 function Poster() {
-  const [index,setindex]=useState(0);
-  const obj={
+  const [index, setIndex] = useState(0);
+
+  // Function to go to the next index
+  function goToNextIndex() {
+    // Calculate the next index in a circular manner
+    const nextIndex = (index + 1) % imageSlide.length;
+    setIndex(nextIndex);
+  }
+
+  // Function to go to the previous index
+  function goToPrevIndex() {
+    // Calculate the previous index in a circular manner
+    const prevIndex = (index - 1 + imageSlide.length) % imageSlide.length;
+    setIndex(prevIndex);
+  }
+
+  const obj = {
     backgroundImage: `url(${imageSlide[index].url})`,
     backgroundPosition: 'center',
     backgroundSize: 'auto',
-    height:'500px',
-    width:"100%",
+    height: '500px',
+    width: "100%",
     backgroundRepeat: 'no-repeat',
     transition: 'background-image 0.4s ease',
-  }
-  function gotonextindex(index)
-  {
-    setindex(index);
-  }
+  };
+
   return (
     <>
-    <div className="counier-style">
+      <div className="counier-style">
         <div style={obj}>
-        <div className="description">
+          <div className="description">
             <div>
               <h1>{imageSlide[index].title}</h1>
               <h1>{imageSlide[index].body}</h1>
             </div>
-            <div className="carousal-boullt">
-              {
-                imageSlide.map((imageSlide,index)=>(
-                    <span 
-                    key={index} 
-                    onClick={()=>{
-                    gotonextindex(index)
-                    }}></span>
-                ))
-              }
+            <div className="carousel-buttons">
+              {/* Arrow button to go to the previous image */}
+              <span
+                className="arrow-button left arrow-box"
+                onClick={goToPrevIndex}
+              >
+                &#8249;
+              </span>
+              {/* Mapping through imageSlide to create buttons for each image */}
+              {imageSlide.map((_, i) => (
+                <span
+                  key={i}
+                  onClick={() => {
+                    // Call the function to go to the clicked index
+                    setIndex(i);
+                  }}
+                ></span>
+              ))}
+              {/* Arrow button to go to the next image */}
+              <span
+                className="arrow-button right arrow-box"
+                onClick={goToNextIndex}
+              >
+                &#8250;
+              </span>
+            </div>
+          </div>
         </div>
-           </div>
-        </div>
-    </div>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default Poster
+export default Poster;
