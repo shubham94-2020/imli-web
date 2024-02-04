082e3c80e8452d6791038b49f5faf983a48cd3f7
maheshthakare2003 <maheshthakare149@gmail.com>
2024-02-05 00:24:04 +0530

    Revert "Added Contact Info Part of Contact Us page"
    This reverts commit 9dc63ade5c4c160b0db692a2c81214953afe9398.

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import './contact.css';
-import { RiInstagramFill } from "react-icons/ri";
-import { FaYoutube } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { IoLocation } from "react-icons/io5";
-import { IoCall } from "react-icons/io5";
-
-
-
+import  CIcon  from '@coreui/icons-react';
+import { cilLocationPin, cilPhone } from '@coreui/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ContactUs = () => {
   return (
@@ -40,29 +35,21 @@ const ContactUs = () => {
         <h1>Contact Info</h1>
           <div className='address'>
             <div className='reg'> 
-            {/* <CIcon icon={cilLocationPin}  className="text-secondary cIcon" size="sm"/> */}
-            <IoLocation className='LocationIcon'/>
+            <CIcon icon={cilLocationPin}  className="text-secondary cIcon" size="sm"/>
             <div className="details">
             <span><b>Registered Office :</b></span>
             <span className='text-body'>Institute for Multi-Lingual Education, Indraprabha Co-op Housing Society, 3rd Floor, Pune-Bangalore Highway, Dehuroad, Pune 412 101</span>
             </div>
             </div>
             <div className="call">
-            {/* <CIcon icon={cilPhone}  className="text-secondary cIcon1" size="sm"/> */}
-            <IoCall className='PhoneIcon'/>
-            <div className="detailsCall">
-            <span><b>Phone :</b></span>
-            <span className='phone'>+91 99609 28210 </span>
-            </div>
+            <CIcon icon={cilPhone}  className="text-secondary cIcon1" size="sm"/>
+            <div className='phone'>+91 99609 28210 </div>
             </div>
 
           </div>
-      <div className="ContactIcons">
-        <FaFacebook className='SocialIcons'/> <FaYoutube className='SocialIconsY'/> <RiInstagramFill className='SocialIcons' />
-
       </div>
       </div>
-      </div>
+      <div className="Contact"></div>
     </div>
   );
 }
