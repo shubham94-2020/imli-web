f5a8d93208d52a20805b5c776c08f29ac97624c6
AshokShekade2003 <ashokshekade543@gmail.com>
2024-03-27 12:41:08 +0530

    contactus and about us updated

--- frontend/components/Contact/Contact.jsx
+++ frontend/components/Contact/Contact.jsx
@@ -37,109 +37,141 @@ const ContactUs = () => {
       );
   };
   return (
-    <div className="contact-us-container">
-      <div className="contact-us-text">
-        <h1>Let’s Collaborate</h1>
-        <p>
-          Would you like to collaborate with us or learn about our work? We are
-          always open for collaborating on innovative projects! Send us an email
-          and we’ll be sure to get back to you.
-        </p>
-        <p>
-          Reach us at{" "}
-          <a href="imli.education@gmail.com">
-            {" "}
-            <b>imli.education@gmail.com</b>
-          </a>
-        </p>
-      </div>
-      <div className="Contact">
-        <div className="contact-us-form">
-          <form ref={form} onSubmit={sendEmail}>
-            {/* <label htmlFor="name">Name:</label> */}
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              required
-            />
-
-            {/* <label htmlFor="email">Email:</label> */}
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email"
-              required
-            />
-
-            {/* <label htmlFor="subject">Subject:</label> */}
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              placeholder="Subject"
-              required
-            />
-
-            {/* <label htmlFor="message">Message:</label> */}
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Message"
-              required
-            ></textarea>
-
-            <button type="submit">Send</button>
-          </form>
+    <>
+      <div className="upperMain">
+        <div className="careers">
+          <div>
+            <h2>Careers</h2>
+            <p>Join our incredible team of dreamers and doers.</p>
+            <button className="chat">
+              {" "}
+              <a href="https://wa.me/919960928210">Positions Open</a>
+            </button>
+          </div>
+          <img src="i1.jpg" alt="img" />
         </div>
-
-        <div className="ContactInfo">
-          <h1>Contact Info</h1>
-          <div className="address">
-            <div className="reg">
-              {/* <CIcon icon={cilLocationPin}  className="text-secondary cIcon" size="sm"/> */}
-              <TiLocation className="LocationIcon" />
-              <div className="details">
-                <span>
-                  <b>Registered Office :</b>
-                </span>
-                <span className="text-body">
-                  Institute for Multi-Lingual Education, Indraprabha Co-op
-                  Housing Society, 3rd Floor, Pune-Bangalore Highway, Dehuroad,
-                  Pune 412 101
-                </span>
-              </div>
-            </div>
-            <div className="call">
-              {/* <CIcon icon={cilPhone}  className="text-secondary cIcon1" size="sm"/> */}
-              <IoCall className="PhoneIcon" />
-              <div className="detailsCall">
-                <span>
-                  <b>Phone :</b>
-                </span>
-                <span className="phone">+91 99609 28210 </span>
-              </div>
-            </div>
+        <div className="corporate">
+          <div>
+            <h2>Corporate Partners</h2>
+            <p>
+              We welcome long term relationships to impact students learning in
+              different languages across public and private systems.
+            </p>
+            <p className="m">
+              Contact :  
+              <a href="imli.education@gmail.com">
+                
+                 imli.education@gmail.com
+              </a>
+            </p>
           </div>
-          <div className="ContactIcons">
-            <SocialIcon
-              url="https://www.facebook.com/imli.education/"
-              target="_blank"
-            />
-            <SocialIcon
-              url="https://www.youtube.com/@imli.education/featured"
-              target="_blank"
-            />
-            <SocialIcon
-              url="https://www.instagram.com/imli.education/"
-              target="_blank"
-            />
-          </div>
+          <img src="i1.jpg" alt="img" />
         </div>
       </div>
-    </div>
+      <div className="contact-us-container">
+        <div className="contact-us-text">
+          <h1>Let’s Collaborate</h1>
+          <p>
+            Would you like to collaborate with us or learn about our work? We
+            are always open for collaborating on innovative projects! Send us an
+            email and we’ll be sure to get back to you.
+          </p>
+          <p>
+            Reach us at{" "}
+            <a href="imli.education@gmail.com">
+              {" "}
+              <b>imli.education@gmail.com</b>
+            </a>
+          </p>
+        </div>
+        <div className="Contact">
+          <div className="contact-us-form">
+            <form ref={form} onSubmit={sendEmail}>
+              {/* <label htmlFor="name">Name:</label> */}
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                required
+              />
+
+              {/* <label htmlFor="email">Email:</label> */}
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+
+              {/* <label htmlFor="subject">Subject:</label> */}
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+
+              {/* <label htmlFor="message">Message:</label> */}
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Message"
+                required
+              ></textarea>
+
+              <button type="submit">Send</button>
+            </form>
+          </div>
+
+          <div className="ContactInfo">
+            <h1>Contact Info</h1>
+            <div className="address">
+              <div className="reg">
+                {/* <CIcon icon={cilLocationPin}  className="text-secondary cIcon" size="sm"/> */}
+                <TiLocation className="LocationIcon" />
+                <div className="details">
+                  <span>
+                    <b>Registered Office :</b>
+                  </span>
+                  <span className="text-body">
+                    Institute for Multi-Lingual Education, Indraprabha Co-op
+                    Housing Society, 3rd Floor, Pune-Bangalore Highway,
+                    Dehuroad, Pune 412 101
+                  </span>
+                </div>
+              </div>
+              <div className="call">
+                {/* <CIcon icon={cilPhone}  className="text-secondary cIcon1" size="sm"/> */}
+                <IoCall className="PhoneIcon" />
+                <div className="detailsCall">
+                  <span>
+                    <b>Phone :</b>
+                  </span>
+                  <span className="phone">+91 99609 28210 </span>
+                </div>
+              </div>
+            </div>
+            <div className="ContactIcons">
+              <SocialIcon
+                url="https://www.facebook.com/imli.education/"
+                target="_blank"
+              />
+              <SocialIcon
+                url="https://www.youtube.com/@imli.education/featured"
+                target="_blank"
+              />
+              <SocialIcon
+                url="https://www.instagram.com/imli.education/"
+                target="_blank"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
